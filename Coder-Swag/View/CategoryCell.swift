8539306299d0d32a5c9638326c925831a2a300c2
Steven <stevenalfred93@yahoo.com>
2018-06-15 00:03:33 -0400

    Added IBOutlets and set up prototype cell in table

---
+++ Coder-Swag/View/CategoryCell.swift
@@ -0,0 +1,27 @@
+//
+//  CategoryCell.swift
+//  Coder-Swag
+//
+//  Created by Steven on 6/14/18.
+//  Copyright © 2018 Steven Santiago. All rights reserved.
+//
+
+import UIKit
+
+class CategoryCell: UITableViewCell {
+    
+    @IBOutlet weak var categoryImage:UIImageView!
+    @IBOutlet weak var categoryTitle: UILabel!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
