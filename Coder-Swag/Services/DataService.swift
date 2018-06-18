47853fc9fb81d7402c1367e73aeac994a217a8be
Steven <stevenalfred93@yahoo.com>
2018-06-18 00:04:34 -0400

    Added Services Folder with DataService file

---
+++ Coder-Swag/Services/DataService.swift
@@ -0,0 +1,13 @@
+//
+//  DataService.swift
+//  Coder-Swag
+//
+//  Created by Steven on 6/18/18.
+//  Copyright © 2018 Steven Santiago. All rights reserved.
+//
+
+import Foundation
+
+class DataService {
+    static let instance = DataService()
+}
